fc44cffb14c47e7111d6c9afc8caec2011c2b851
Tiago <Tiagolch@hotmail.com>
2021-01-13 12:37:21 -0300

    Adicionado autenticação

--- tel/views.py
+++ tel/views.py
@@ -1,11 +1,19 @@
 from rest_framework import viewsets
 from .models import Telefone
 from .serializer import TelefoneSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class TelefoneViewSet(viewsets.ModelViewSet):
     queryset = Telefone.objects.all()
     serializer_class = TelefoneSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+
+
 
 
 
