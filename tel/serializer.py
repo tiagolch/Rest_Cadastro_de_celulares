6435f3c8e289327973b30ee5083247816c7a111e
Tiago <Tiagolch@hotmail.com>
2021-01-14 12:40:22 -0300

    Adicionado validação no campo mac pelo Serializer

--- tel/serializer.py
+++ tel/serializer.py
@@ -5,5 +5,9 @@ from tel.models import Telefone
 class TelefoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Telefone
-        fields = ['numero', 'modelo', 'serial','imei1', 'imei2', 'setor','conta', 'senha', 'mac']
+        fields = ['id','numero', 'modelo', 'serial','imei1', 'imei2', 'setor','conta', 'senha', 'mac']
 
+    def validate_mac(self, mac):
+        if len(mac) != 17:
+            raise serializers.ValidationError("O Campo 'Mac' deve ter 17 caracteres")
+        return mac
